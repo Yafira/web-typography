476314a6f5da9152438128cd7407919dffeebe86
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-04 21:43:38 -0400

    preloaded a font to use

--- 01 01/script.js
+++ 01 01/script.js
@@ -1,3 +1,9 @@
+let font
+
+function preload() {
+    font = loadFont("assets/spacegrotesk-medium.otf")
+}
+
 function setup() {
     createCanvas(500, 500);
 }
@@ -6,6 +12,7 @@ function draw() {
     background("#fbfcc0")
 
     fill("#e21fb1")
+    textFont(font)
     textSize(400)
     textAlign(CENTER, CENTER)
     text("01", width / 2, height / 2)
