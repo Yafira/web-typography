32240ccd0007706cf78a9c7008a793b678fc8f31
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-04 21:58:42 -0400

    implemented createGraphics and copy

--- 01 01/script.js
+++ 01 01/script.js
@@ -1,4 +1,5 @@
 let font
+let graphic
 
 function preload() {
     font = loadFont("assets/spacegrotesk-medium.otf")
@@ -6,14 +7,21 @@ function preload() {
 
 function setup() {
     createCanvas(500, 500);
+
+
+    graphic = createGraphics(500, 500)
+
+
+    graphic.fill("#e21fb1")
+    graphic.textFont(font)
+    graphic.textSize(400)
+    graphic.textAlign(CENTER, CENTER)
+    graphic.text("01", width / 2, height / 2)
 }
 
 function draw() {
     background("#fbfcc0")
 
-    fill("#e21fb1")
-    textFont(font)
-    textSize(400)
-    textAlign(CENTER, CENTER)
-    text("01", width / 2, height / 2)
+    copy(graphic, 0, 0, 500, 500, 0, 0, frameCount, 500)
+    copy(graphic, 0, 0, 500, 500, frameCount, 0, 500 - frameCount, 500)
 } 
