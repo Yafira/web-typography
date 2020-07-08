abaa0d52d78c37a7c4ac781633ff4e5db378fc55
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-08 00:28:07 -0400

    implemented a for loop

--- aaaaaaaaa/script.js
+++ aaaaaaaaa/script.js
@@ -1,21 +1,27 @@
-let font 
+let font
+let graphic
 
 function preload () {
     font = loadFont("assets/spacegrotesk-medium.otf")
 }
 
 
-
 function setup() {
     createCanvas(600, 600)
 
-    fill("#ac90ec")
-    textSize(800)
-    textAlign(CENTER, CENTER)
-    text("a", 300, 240)
+    graphic = createGraphics(600, 600)
+
+    graphic.fill("#ac90ec")
+    graphic.textFont(font)
+    graphic.textSize(800)
+    graphic.textAlign(CENTER, CENTER)
+    graphic.text("a", 300, 240)
 }
 
 function draw() {
-    
 
+
+    for (let x = 0; x < 12; x = x + 1) {
+        copy(graphic, 0, 0, 600, 600, x * 50, 0, 50, 600)
+    }
 }
