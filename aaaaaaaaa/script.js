0198d765cb2ca19929285a03616c09c854be2cac
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-08 00:45:50 -0400

    recreating the original text with source and destination points

--- aaaaaaaaa/script.js
+++ aaaaaaaaa/script.js
@@ -23,7 +23,19 @@ function draw() {
 
     for (let x = 0; x < 12; x = x + 1) {
         for (let y = 0; y < 12; y = y + 1) {
-            image(graphic, x * tileSize, y * tileSize, tileSize, tileSize, 0, 0, 600, 600)
+            // source 
+            const sx = x * tileSize
+            const sy = y * tileSize
+            const sw = tileSize
+            const sh = tileSize
+
+            // destination
+            const dx = x * tileSize
+            const dy = y * tileSize
+            const dw = tileSize
+            const dh = tileSize
+
+            image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
             // copy = (source, destination)
             // image = (destination, source)
         }     
