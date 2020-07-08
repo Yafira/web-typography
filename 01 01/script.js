061973d32faffb23fa9836933e35514f87b2225c
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-08 01:14:14 -0400

    implemented another variation by drawing a second graphic 01 01 -> 01 23

--- 01 01/script.js
+++ 01 01/script.js
@@ -1,5 +1,7 @@
 let font
 let graphic
+// variation B: 01 23
+let graphic2
 
 function preload() {
     font = loadFont("assets/spacegrotesk-medium.otf")
@@ -17,6 +19,17 @@ function setup() {
     graphic.textSize(400)
     graphic.textAlign(CENTER, CENTER)
     graphic.text("01", width / 2, height / 2)
+
+
+    graphic2 = createGraphics(500, 500)
+
+
+    graphic2.fill("#e21fb1")
+    graphic2.textFont(font)
+    graphic2.textSize(400)
+    graphic2.textAlign(CENTER, CENTER)
+    graphic2.text("23", width / 2, height / 2)
+
 }
 
 function draw() {
@@ -24,6 +37,6 @@ function draw() {
 
     let val = sin(frameCount * 0.05) * 250
 
-    copy(graphic, 0, 0, 500, 500, 0, 0, 250 + val, 500)
-    copy(graphic, 0, 0, 500, 500, 250 + val, 0, 250 - val, 500)
-} 
+    copy(graphic, 0, 0, 250 + val, 500, 0, 0, 250 + val, 500)
+    copy(graphic2, 250 + val, 0, 250 - val, 500, 250 + val, 0, 250 - val, 500)
+}
