3c7dd3a1010421e04add2209a307b9d4872c09ff
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-10 00:08:06 -0400

    cut and paste with createGraphics

--- Good Morning/script.js
+++ Good Morning/script.js
@@ -1,4 +1,5 @@
 let font
+let graphic
 
 function preload () {
     font = loadFont("assets/spacegrotesk-medium.otf")
@@ -8,16 +9,18 @@ function preload () {
 function setup() {
     createCanvas(1200, 600)
 
-    fill("#ff0000")
-    textSize(300)
-    textAlign(CENTER, CENTER)
-    textFont(font)
-    textLeading(200)
-    text("Good/nMorning", 600, 300)
+    graphic = createGraphics(1200, 600)
+
+    graphic.fill("#ff0000")
+    graphic.textSize(300)
+    graphic.textAlign(CENTER, CENTER)
+    graphic.textFont(font)
+    graphic.textLeading(200)
+    graphic.text("Good\nMorning", 600, 300)
 }
 
 function draw() {
     background("#ebe2d8")
+    image(graphic, frameCount, 0, 1200, 600, 0, 0, 1200, 600)
 
-
-}     
+}
