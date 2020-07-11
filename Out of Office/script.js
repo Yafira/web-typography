a3147380f44a39f828a80452f069e069684b63f5
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-10 20:45:49 -0400

    cloning text twelve times

--- Out of Office/script.js
+++ Out of Office/script.js
@@ -20,6 +20,32 @@ function setup() {
 
 
 function draw() {
-    image(graphic, 0, 0, 1200, 600, 0, 0, 1200, 600)
+    background("#000000")
+
+    
+    const tileSize = 50
+
+    for (let y = 0; y < 0; y = y + 1) {
+
+        // source
+        const sx = 0
+        const sy = 0
+        const sw = 1200
+        const sh = 600
+
+        // destination 
+        const dx = 0
+        const dy = y * tileSize
+        const dw = 1200
+        const dh = tileSize
+
+
+
+        image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
+    }
+
+
+
+
 }
 
