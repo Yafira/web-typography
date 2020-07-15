222ab113ffc14246007480ee1747589a9505e190
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-15 00:59:51 -0400

    added in random dots

--- Brutalust/script.js
+++ Brutalust/script.js
@@ -2,18 +2,26 @@ const headerTags = document.querySelectorAll("h1, h2")
 
 const runRandom = tag => {
     const originalContent = tag.innerHTML
-
+    let newContent = ""
     let num = 0
 
     const addInterval = setInterval(() => {
         num = num + 1
-        tag.innerHTML = originalContent.substring(0, num)
+        newContent = originalContent.substring(0, num)
 
         if (originalContent == tag.innerHTML) {
             clearInterval(addInterval)
         }
-
     }, 100)
+
+    const randomInterval = setInterval(() => {
+        tag.innerHTML = newContent
+
+        for (let i = newContent.length; i < originalContent.length; i = i + 1) {
+            tag.innerHTML = tag.innerHTML + "."
+        }
+
+    }, 50)
 
 
 }
