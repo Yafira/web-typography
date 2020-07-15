263e76eb2027b0e750fa0a4fd07caeb72be7e0e3
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-15 16:06:38 -0400

    drawing shapes with the points

--- Text to Points/script.js
+++ Text to Points/script.js
@@ -11,7 +11,7 @@ function setup() {
 
 
     points = font.textToPoints("Hello World!", 130, 330, 180, {
-        sampleFactor: 0.1, 
+        sampleFactor: 0.1, // ratio of path-length to number of samples, higher values yield more points
         simplifyThreshold: 0
     })
 }
@@ -27,4 +27,17 @@ function draw() {
     points.forEach(point => {
         circle(point.x, point.y, 5)
     })
+
+
+    noFill()
+    stroke("#b617ff")
+
+    beginShape()
+
+    points.forEach(point => {
+        vertex(point.x, point.y)
+    })
+    endShape()
+
+
 }
