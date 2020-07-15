cdf97f7afc4577137fded6435eeec82b2f65b1b5
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-15 15:59:22 -0400

    implemented p5's textToPoints

--- Text to Points/script.js
+++ Text to Points/script.js
@@ -1,5 +1,5 @@
 let font
- 
+let points
 
 function preload() {
     font = loadFont("assets/inconsolata-bold.ttf")
@@ -8,10 +8,23 @@ function preload() {
 
 function setup() {
     createCanvas(1200, 600)
+
+
+    points = font.textToPoints("Hello World!", 130, 330, 180, {
+        sampleFactor: 0.1, 
+        simplifyThreshold: 0
+    })
 }
 
 
 function draw() {
     background("#eef3d7")
-    text("Hello World!", 100, 100)
+
+    fill("#b617ff")
+    noStroke()
+
+
+    points.forEach(point => {
+        circle(point.x, point.y, 5)
+    })
 }
