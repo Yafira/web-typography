4610aed4dee20240619eab04464321232595ea63
Yafira <yafira.martinez@lc.cuny.edu>
2020-07-15 01:05:28 -0400

    added in random characters

--- Brutalust/script.js
+++ Brutalust/script.js
@@ -4,6 +4,7 @@ const runRandom = tag => {
     const originalContent = tag.innerHTML
     let newContent = ""
     let num = 0
+    const randomList = "abcdefghijklmnopqrstuvwxyz._$£@!".split("")
 
     const addInterval = setInterval(() => {
         num = num + 1
@@ -11,6 +12,7 @@ const runRandom = tag => {
 
         if (originalContent == tag.innerHTML) {
             clearInterval(addInterval)
+            clearInterval(randomInterval)
         }
     }, 100)
 
@@ -18,7 +20,7 @@ const runRandom = tag => {
         tag.innerHTML = newContent
 
         for (let i = newContent.length; i < originalContent.length; i = i + 1) {
-            tag.innerHTML = tag.innerHTML + "."
+            tag.innerHTML = tag.innerHTML + randomList[Math.floor(Math.random() * randomList.length)]
         }
 
     }, 50)
